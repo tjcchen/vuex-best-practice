dd0d613ac972c1de717cf891ab5815a5e307e5e5
Andy Chen <chenyangpoppin@hotmail.com>
2021-01-14 15:43:51 +0800

    feat: add a simple vuex project

---
+++ simple/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img class="header-logo" alt="Vue logo" src="./assets/logo.png">
+    <div class="status">
+      {{ greeting }} {{ loadingStatus }}
+    </div>
+    <div class="fecth-btn">
+      <button @click="fetchTodos()">fetchData</button>
+    </div>
+    <div class="display-data">
+      {{ todos }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data: function() {
+    return {
+      greeting: 'hello vuex!'
+    };
+  },
+  methods: {
+    fetchTodos: function() {
+      this.$store.dispatch('fetchTodos');
+    },
+  },
+  computed: {
+    loadingStatus() {
+      return this.$store.state.loadingStatus
+    },
+    todos() {
+      return this.$store.state.todos
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app .header-logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ simple/src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex';
+import axios from 'axios';
+import App from './App.vue'
+
+Vue.use(Vuex);
+Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    loadingStatus: 'notLoading',
+    todos: []
+  },
+  mutations: {
+    SET_LOADING_STATUS(state, status) {
+      state.loadingStatus = status;
+    },
+    SET_TODOS(state, todos) {
+      state.todos = todos;
+    }
+  },
+  actions: {
+    fetchTodos(context) {
+      context.commit('SET_LOADING_STATUS', 'loading');
+
+      axios.get('http://www.colr.org/json/color/random').then(response => {
+        context.commit('SET_LOADING_STATUS', 'notLoading');
+        context.commit('SET_TODOS', response.data.colors);
+      });
+    }
+  }
+});
+
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app')
