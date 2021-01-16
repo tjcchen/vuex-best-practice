d8d1bb1d4d9738c726d209c66b1c1c0e0cd4b1ac
Andy Chen <chenyangpoppin@hotmail.com>
2021-01-16 21:49:11 +0800

    fix: fix username info

--- main/src/App.vue
+++ main/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="basic-info">
       <h3>basic information</h3>
-      <div>{{ greeting }} : {{ email }}</div>
+      <div>{{ username }} : {{ email }}</div>
       <div>Website: {{ website }}</div>
     </div>
     <hr/>
@@ -26,7 +26,7 @@ export default {
   },
   data: function() {
     return {
-      greeting: 'Vuex'
+      username: 'Username'
     };
   },
   computed: mapState({
